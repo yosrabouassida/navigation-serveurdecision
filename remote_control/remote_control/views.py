ecd78e53a28f6648a8bdeec2937645945163312e
y <yosra.bouassida16@gmail.com>
2020-07-06 13:08:56 +0200

    second commit

--- remote_control/remote_control/views.py
+++ remote_control/remote_control/views.py
@@ -69,54 +69,15 @@ def run(request):
 			fw.turn(int(action.split(':')[1]))
 		
 		# ================ Camera =================
-		elif action == 'camready':
-			cam.ready()
-		elif action == "camleft":
-			cam.turn_left(40)
-		elif action == 'camright':
-			cam.turn_right(40)
-		elif action == 'camup':
-			cam.turn_up(20)
-		elif action == 'camdown':
-			cam.turn_down(20)	
-	if 'speed' in request.GET:
-		speed = int(request.GET['speed'])
-		if speed < 0:
-			speed = 0
-		if speed > 100:
-			speed = 100
-		SPEED = speed
-		if bw_status != 0:
-			bw.speed = SPEED
-		debug = "speed =", speed
+		
 	host = stream.get_host().decode('utf-8').split(' ')[0]
 	return render_to_response("run.html", {'host': host})
 
 def cali(request):
 	if 'action' in request.GET:
 		action = request.GET['action']
-		# ========== Camera calibration =========
-		if action == 'camcali':
-			print('"%s" command received' % action)
-			cam.calibration()
-		elif action == 'camcaliup':
-			print('"%s" command received' % action)
-			cam.cali_up()
-		elif action == 'camcalidown':
-			print('"%s" command received' % action)
-			cam.cali_down()
-		elif action == 'camcalileft':
-			print('"%s" command received' % action)
-			cam.cali_left()
-		elif action == 'camcaliright':
-			print('"%s" command received' % action)
-			cam.cali_right()
-		elif action == 'camcaliok':
-			print('"%s" command received' % action)
-			cam.cali_ok()
-
 		# ========= Front wheel cali ===========
-		elif action == 'fwcali':
+		if action == 'fwcali':
 			print('"%s" command received' % action)
 			fw.calibration()
 		elif action == 'fwcalileft':
