ec6b9705ade951a67afa934469d4226be3e1e5a4
y <yosra.bouassida16@gmail.com>
2020-07-06 13:38:59 +0200

    forth commit

--- remote_control/remote_control/views.py
+++ remote_control/remote_control/views.py
@@ -20,8 +20,7 @@ picar.setup()
 db_file = "/home/pi/SunFounder_PiCar-V/remote_control/remote_control/driver/config"
 fw = front_wheels.Front_Wheels(debug=False, db=db_file)
 bw = back_wheels.Back_Wheels(debug=False, db=db_file)
-cam = camera.Camera(debug=False, db=db_file)
-cam.ready()
+
 bw.ready()
 fw.ready()
  
