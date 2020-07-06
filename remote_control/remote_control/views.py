74829b46f7586a5a1f0251e8d6f8a4b136868e8d
y <yosra.bouassida16@gmail.com>
2020-07-06 12:24:33 +0200

    thirdperception

--- remote_control/remote_control/views.py
+++ remote_control/remote_control/views.py
@@ -33,36 +33,7 @@ print(stream.start())
 def home(request):
 	return render_to_response("base.html")
 
-def run(request):
-	global SPEED, bw_status
-	debug = ''
-	if 'action' in request.GET:
-		action = request.GET['action']
-		
-		
-		# ================ Camera =================
-		if action == 'camready':
-			cam.ready()
-		elif action == "camleft":
-			cam.turn_left(40)
-		elif action == 'camright':
-			cam.turn_right(40)
-		elif action == 'camup':
-			cam.turn_up(20)
-		elif action == 'camdown':
-			cam.turn_down(20)	
-	if 'speed' in request.GET:
-		speed = int(request.GET['speed'])
-		if speed < 0:
-			speed = 0
-		if speed > 100:
-			speed = 100
-		SPEED = speed
-		if bw_status != 0:
-			bw.speed = SPEED
-		debug = "speed =", speed
-	host = stream.get_host().decode('utf-8').split(' ')[0]
-	return render_to_response("run.html", {'host': host})
+
 
 def cali(request):
 	if 'action' in request.GET:
@@ -87,7 +58,36 @@ def cali(request):
 			print('"%s" command received' % action)
 			cam.cali_ok()
 
-	
+		# ========= Front wheel cali ===========
+		elif action == 'fwcali':
+			print('"%s" command received' % action)
+			fw.calibration()
+		elif action == 'fwcalileft':
+			print('"%s" command received' % action)
+			fw.cali_left()
+		elif action == 'fwcaliright':
+			print('"%s" command received' % action)
+			fw.cali_right()
+		elif action == 'fwcaliok':
+			print('"%s" command received' % action)
+			fw.cali_ok()
+
+		# ========= Back wheel cali ===========
+		elif action == 'bwcali':
+			print('"%s" command received' % action)
+			bw.calibration()
+		elif action == 'bwcalileft':
+			print('"%s" command received' % action)
+			bw.cali_left()
+		elif action == 'bwcaliright':
+			print('"%s" command received' % action)
+			bw.cali_right()
+		elif action == 'bwcaliok':
+			print('"%s" command received' % action)
+			bw.cali_ok()
+		else:
+			print('command error, error command "%s" received' % action)
+	return render_to_response("cali.html")
 
 def connection_test(request):
 	return HttpResponse('OK')
